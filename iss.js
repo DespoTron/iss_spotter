af27694cbe03c58944c21691ac6639d9091238cb
Thai Do <thai.hl.do@gmail.com>
2020-08-27 13:14:01 +0000

    Replaced a console.log for a callback instead

--- iss.js
+++ iss.js
@@ -11,21 +11,25 @@ const request = require('request');
 const url = 'https://api.ipify.org?format=json';
 
 const fetchMyIP = function(callback) {
+  // insde the request callback ...
+  // error can be set if invalid domain, user is offline etc...
   // use request to fetch IP address from JSON API
   request(url, (error, response, body) => {
     if (error) {
       callback(error, null);
       return;
     }
-
+    
+    // if non-200 status, assume server error
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response ${body}`;
       callback(Error(msg), null);
       return;
     }
 
-    const data = JSON.parse(body);
-    console.log(data);
+    // if we get here, all's well and we got the data
+    const data = JSON.parse(body).ip;
+    callback(null, data);
 
   });
 
