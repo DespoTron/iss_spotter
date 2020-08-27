fb26bf89af2cbfd1163c59260628b7e27db2aa18
Thai Do <thai.hl.do@gmail.com>
2020-08-27 13:34:07 +0000

    added the fetchCoordsByIP function

--- iss.js
+++ iss.js
@@ -11,7 +11,7 @@ const request = require('request');
 const url = 'https://api.ipify.org?format=json';
 
 const fetchMyIP = function(callback) {
-  // insde the request callback ...
+  // inside the request callback ...
   // error can be set if invalid domain, user is offline etc...
   // use request to fetch IP address from JSON API
   request(url, (error, response, body) => {
@@ -22,7 +22,7 @@ const fetchMyIP = function(callback) {
     
     // if non-200 status, assume server error
     if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP. Response ${body}`;
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response ${body},`;
       callback(Error(msg), null);
       return;
     }
@@ -35,4 +35,24 @@ const fetchMyIP = function(callback) {
 
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request('https://ipvigilante.com/8.8.8.8', (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status code ${response.statusCode} when fetching coordinates IP. Response ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    console.log(data);
+
+  });
+}
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
